46de47c807c87eb0034756be3e640a64c1209a8b
katherineh <444kath@gmail.com>
2017-07-08 15:52:27 -0700

    Add hardcoded example of Google search intent

--- app/src/main/java/com/example/admin/voiciferous/MainActivity.java
+++ app/src/main/java/com/example/admin/voiciferous/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.admin.voiciferous;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<String> searchWordList = new ArrayList<String>();
+
+        // Here is a hard coded example of the parsed results received from Watson
+        searchWordList.add("movie");
+        searchWordList.add("1 hour");
+        searchWordList.add("family");
+        searchWordList.add("G-rated");
+        searchWordList.add("dogs");
+
+
+        Intent searchIntent = new Intent(Intent.ACTION_WEB_SEARCH);
+        String keyword= searchWordList.get(1);
+        searchIntent.putExtra(SearchManager.QUERY, keyword);
+        if (searchIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(searchIntent);
+        }
+
+
+
     }
+
 }
+
